05d714d47791e36a38f7e8e51e3cecd19db76273
Manuel Lépori <mlepori@gmail.com>
2013-09-18 15:00:58 -0300

    terminado la primera aproximacion

--- migra_csv_contacts.php
+++ migra_csv_contacts.php
@@ -1,55 +1,54 @@
 <?php
-/***************************************************
-*Genera un archivo .sql para importar la tabla account de sugar 4.2.x a 6.5.x.
-*No se porque corta los campos cuando encuentra una tilde....
-*
-*
-/***************************************************/
-?>
-<?php
-/*
-Estructura de Accounts en sugar 4.2.1:
-----------------------------------------------------------------------
+/*----------------------------------------
+Estructura de Contacts en 4.2
+
 id
+deleted
 date_entered
 date_modified
 modified_user_id
 assigned_user_id
 created_by
-name
-parent_id
-account_type
-industry
-annual_revenue
+salutation
+first_name
+last_name
+lead_source
+title
+department
+reports_to_id
+birthdate
+do_not_call // no importar
+phone_home
+phone_mobile
+phone_work
+phone_other
 phone_fax
-billing_address_street
-billing_address_city
-billing_address_state
-billing_address_postalcode
-billing_address_country
+email1
+email2
+assistant
+assistant_phone
+email_opt_out // no importar
+primary_address_street
+primary_address_city
+primary_address_state
+primary_address_postalcode
+primary_address_country
+alt_address_street
+alt_address_city
+alt_address_state
+alt_address_postalcode
+alt_address_country
 description
-rating
-phone_office
-phone_alternate
-email1 <---------- No está en 6.5.x
-email2 <---------- No está en 6.5.x
-website
-ownership
-employees
-sic_code
-ticker_symbol
-shipping_address_street
-shipping_address_city
-shipping_address_state
-shipping_address_postalcode
-shipping_address_country
-deleted
+portal_name
+portal_active // no importar
+portal_app
+invalid_email // no importar
 
-Estructura de Accounts en sugar 6.5.15
-----------------------------------------------------------------------
+
+-----------------------------------------
+Estructura de Contacts en 6.5
 
 id
-name
 date_entered
 date_modified
 modified_user_id
@@ -57,35 +56,37 @@ created_by
 description
 deleted
 assigned_user_id
-account_type
-industry
-annual_revenue
+salutation
+first_name
+last_name
+title
+department
+do_not_call // no importar
+phone_home
+phone_mobile
+phone_work
+phone_other
 phone_fax
-billing_address_street
-billing_address_city
-billing_address_state
-billing_address_postalcode
-billing_address_country
-rating
-phone_office
-phone_alternate
-website
-ownership
-employees
-ticker_symbol
-shipping_address_street
-shipping_address_city
-shipping_address_state
-shipping_address_postalcode
-shipping_address_country
-parent_id
-sic_code
-campaign_id <---------- Agregado en 6.5 ; no está en 4.2 
-
-*/
+primary_address_street
+primary_address_city
+primary_address_state
+primary_address_postalcode
+primary_address_country
+alt_address_street
+alt_address_city
+alt_address_state
+alt_address_postalcode
+alt_address_country
+assistant
+assistant_phone
+lead_source
+reports_to_id
+birthdate
+campaign_id
+/*----------------------------------------*/
 require_once("databases.php");
 
-$query = "SELECT * FROM accounts ";
+$query = "SELECT * FROM contacts ";
 $query .= "WHERE deleted = 0";
 
 
@@ -95,12 +96,13 @@ $query .= "WHERE deleted = 0";
 			die("Database query failed: " . mysql_error());
 		}
 
-echo "\"id\",\"name\",\"date_entered\",\"date_modified\",\"modified_user_id\",\"created_by\",\"description\",\"deleted\",\"assigned_user_id\",\"account_type\",\"industry\",\"annual_revenue\",\"phone_fax\",\"billing_address_street\",\"billing_address_city\",\"billing_address_state\",\"billing_address_postalcode\",\"billing_address_country\",\"rating\",\"phone_office\",\"phone_alternate\",\"website\",\"ownership\",\"employees\",\"ticker_symbol\",\"shipping_address_street\",\"shipping_address_city\",\"shipping_address_state\",\"shipping_address_postalcode\",\"shipping_address_country\",\"parent_id\",\"sic_code\",\"campaign_id\"";
+echo "\"id\",\"date_entered\",\"date_modified\",\"modified_user_id\",\"created_by\",\"description\",\"deleted\",\"assigned_user_id\",\"salutation\",\"first_name\",\"last_name\",\"title\",\"department\",\"phone_home\",\"phone_mobile\",\"phone_work\",\"phone_other\",\"primary_address_street\",\"primary_address_city\",\"primary_address_state\",\"primary_address_postalcode\",\"primary_address_country\",\"alt_address_street\",\"alt_address_city\",\"alt_address_state\",\"alt_address_postalcode\",\"alt_address_country\",\"assistant\",\"assistant_phone\",\"lead_source\",\"reports_to_id\",\"birthdate\",\"\",\"phone_fax\",\"email1\",\"email2\",\"portal_name\",\"portal_app\"";
 echo "\n";
+
 	while ($row_query = mysql_fetch_array($result_query)) {
 
+
 			echo "\"" . $row_query['id'] . "\", ";
-			echo "\"" . $row_query['name'] . "\", ";
 			echo "\"" . $row_query['date_entered'] . "\", ";
 			echo "\"" . $row_query['date_modified'] . "\", ";
 			echo "\"" . $row_query['modified_user_id'] . "\", ";
@@ -108,30 +110,43 @@ echo "\n";
 			echo "\"" . $row_query['description'] . "\", ";
 			echo "\"" . $row_query['deleted'] . "\", ";
 			echo "\"" . $row_query['assigned_user_id'] . "\", ";
-			echo "\"" . $row_query['account_type'] . "\", ";
-			echo "\"" . $row_query['industry'] . "\", ";
-			echo "\"" . $row_query['annual_revenue'] . "\", ";
+			echo "\"" . $row_query['salutation'] . "\", ";
+			echo "\"" . $row_query['first_name'] . "\", ";
+			echo "\"" . $row_query['last_name'] . "\", ";
+			echo "\"" . $row_query['title'] . "\", ";
+			echo "\"" . $row_query['department'] . "\", ";
+//			echo "\"" . $row_query['do_not_call '] . "\", "; No se incluye por irrelevante.
+			echo "\"" . $row_query['phone_home'] . "\", ";
+			echo "\"" . $row_query['phone_mobile'] . "\", ";
+			echo "\"" . $row_query['phone_work'] . "\", ";
+			echo "\"" . $row_query['phone_other'] . "\", ";
+			echo "\"" . $row_query['primary_address_street'] . "\", ";
+			echo "\"" . $row_query['primary_address_city'] . "\", ";
+			echo "\"" . $row_query['primary_address_state'] . "\", ";
+			echo "\"" . $row_query['primary_address_postalcode'] . "\", ";
+			echo "\"" . $row_query['primary_address_country'] . "\", ";
+
+			echo "\"" . $row_query['alt_address_street'] . "\", ";
+			echo "\"" . $row_query['alt_address_city'] . "\", ";
+			echo "\"" . $row_query['alt_address_state'] . "\", ";
+			echo "\"" . $row_query['alt_address_postalcode'] . "\", ";
+			echo "\"" . $row_query['alt_address_country'] . "\", ";
+
+			echo "\"" . $row_query['assistant'] . "\", ";
+			echo "\"" . $row_query['assistant_phone'] . "\", ";
+			echo "\"" . $row_query['lead_source'] . "\", ";
+			echo "\"" . $row_query['reports_to_id'] . "\", ";
+			echo "\"" . $row_query['birthdate'] . "\", ";
+			echo "\"\", "; // campaign_id
+
 			echo "\"" . $row_query['phone_fax'] . "\", ";
-			echo "\"" . $row_query['billing_address_street'] . "\", ";
-			echo "\"" . $row_query['billing_address_city'] . "\", ";
-			echo "\"" . $row_query['billing_address_state'] . "\", ";
-			echo "\"" . $row_query['billing_address_postalcode'] . "\", ";
-			echo "\"" . $row_query['billing_address_country'] . "\", ";
-			echo "\"" . $row_query['rating'] . "\", ";
-			echo "\"" . $row_query['phone_office'] . "\", ";
-			echo "\"" . $row_query['phone_alternate'] . "\", ";
-			echo "\"" . $row_query['website'] . "\", ";
-			echo "\"" . $row_query['ownership'] . "\", ";
-			echo "\"" . $row_query['employees'] . "\", ";
-			echo "\"" . $row_query['ticker_symbol'] . "\", ";
-			echo "\"" . $row_query['shipping_address_street'] . "\", ";
-			echo "\"" . $row_query['shipping_address_city'] . "\", ";
-			echo "\"" . $row_query['shipping_address_state'] . "\", ";
-			echo "\"" . $row_query['shipping_address_postalcode'] . "\", ";
-			echo "\"" . $row_query['shipping_address_country'] . "\", ";
-			echo "\"" . $row_query['parent_id'] . "\", ";
-			echo "\"" . $row_query['sic_code'] . "\", ";
-			echo "\"\""; //campaign_id
+			echo "\"" . $row_query['email1'] . "\", ";
+			echo "\"" . $row_query['email2'] . "\", ";
+//			echo "\"" . $row_query['email_opt_out '] . "\", "; //no está en sugar 6_5 y además no nos sirve.
+			echo "\"" . $row_query['portal_name'] . "\", ";
+//			echo "\"" . $row_query['portal_active '] . "\", "; //no está en sugar 6_5 y además no nos sirve.
+			echo "\"" . $row_query['portal_app'] . "\", ";
+//			echo "\"" . $row_query['invalid_email '] . "\", ";//no está en sugar 6_5 y además no nos sirve.
 			echo "\n";
 	}
 
